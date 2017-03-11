43f7aa798e14446d5145661285b5166577ec0b38
Brannon N. Darby II <bdarby86@gmail.com>
2017-03-11 15:57:43 -0500

    loopback config

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoopBackConfig } from './shared/sdk/index';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
     <ng2-toasty></ng2-toasty>
   `,
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor() {
+    LoopBackConfig.setBaseURL('http://127.0.0.1:3000');
+    LoopBackConfig.setApiVersion('api');
+  }
+}
