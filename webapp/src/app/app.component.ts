f1e2b3536a336e3c407af6e5fbc668a687069041
Brannon N. Darby II <bdarby86@gmail.com>
2017-03-11 16:14:53 -0500

    Revert "loopback config"
    This reverts commit 43f7aa798e14446d5145661285b5166577ec0b38.

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { LoopBackConfig } from './shared/sdk/index';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +7,4 @@ import { LoopBackConfig } from './shared/sdk/index';
     <ng2-toasty></ng2-toasty>
   `,
 })
-export class AppComponent {
-  constructor() {
-    LoopBackConfig.setBaseURL('http://127.0.0.1:3000');
-    LoopBackConfig.setApiVersion('api');
-  }
-}
+export class AppComponent { }
