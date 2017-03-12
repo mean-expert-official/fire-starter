702b80ae5e8bcf6cedb2f3fd4d50d57cc7b0e6f5
Brannon N. Darby II <bdarby86@gmail.com>
2017-03-12 16:07:52 -0400

    Add mongodb connector and define use optionally as datasource
    * mongodb connector and datasource
    
    * datasources.local.json
    
    * datasources.local.js
    
    * rebase
    
    * datasources.local.json
    
    * remove datasources.local.json
    
    * mongodb connector and datasource
    
    * datasources.local.json
    
    * rebase
    
    * remove datasources.local.json
    
    * Merge remote-tracking branch 'origin/local-mongodb-datasource' into local-mongodb-datasource
    
    # Conflicts:
    #	fireloop/package.json
    
    * fix
    
    * clean up

---
+++ fireloop/server/datasources.local.js
@@ -0,0 +1,13 @@
+'use strict'
+const mongodbUrl = process.env.MONGODB_URL;
+
+if (mongodbUrl) {
+  console.log('Data sources: Using MongoDB config', mongodbUrl)
+  module.exports = {
+    db: {
+      name: 'db',
+      connector: 'mongodb',
+      url: mongodbUrl,
+    },
+  }
+}
