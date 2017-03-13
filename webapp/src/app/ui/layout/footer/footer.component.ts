83a321827ff1fe764a364a9d789f5751f6288fd1
Brannon N. Darby II <bdarby86@gmail.com>
2017-03-12 21:20:59 -0400

    fireloop branding updates

--- webapp/src/app/ui/layout/footer/footer.component.ts
+++ webapp/src/app/ui/layout/footer/footer.component.ts
@@ -1,28 +1,13 @@
 import { Component } from '@angular/core';
+import { UIService } from '../../ui.service';
 
 @Component({
   selector: 'app-footer',
-  template: `
-    <footer class="footer">
-      <div class="container">
-        <span class="text-muted">
-          <a href="https://github.com/mean-expert-official/fireloop-demo-app">mean-expert-official/fireloop-demo-app</a>
-        </span>
-      </div>
-    </footer>
-  `,
-  styles: [`
-    .footer {
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-      height: 60px;
-      line-height: 60px;
-      background-color: #f5f5f5;
-      text-align: center;
-    }
-  `]
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  constructor(private uiService: UIService) {
 
+  }
 }
